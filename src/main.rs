26840889453da052366a0e26c16fa0b5f9cbf882
Nicolai Kellerer <nicolai.kellerer@kit.edu>
2025-06-05 22:50:46 +0200

    Parse Syslog message

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::net::SocketAddr;
 
 use anyhow::{Context, Result};
+use chrono::Datelike;
 use clap::{arg, Command, value_parser};
 use dotenv::dotenv;
 use redis::{AsyncCommands, streams::StreamMaxlen};
@@ -9,10 +10,69 @@ use tokio::net::UdpSocket;
 use tokio::runtime::Runtime;
 use tokio::sync::mpsc;
 use log::error;
+use serde_json::json;
 
-async fn syslog_worker(mut rx: mpsc::Receiver::<(Vec<u8>, SocketAddr)>, mut redis_client: redis::aio::MultiplexedConnection, stream_key: String, maxlen: usize) -> () {
-    while let Some((msg, addr)) = rx.recv().await {
-        let items = vec![("msg".to_string(), msg), ("addr".to_string(), addr.to_string().into())];
+fn resolve_year((_month, _date, _hour, _min, _sec): syslog_loose::IncompleteDate) -> i32 {
+    let now = chrono::Utc::now();
+    now.year()
+}
+
+async fn syslog_worker<Tz: chrono::TimeZone>(mut rx: mpsc::Receiver::<(chrono::DateTime<Tz>, Vec<u8>, SocketAddr)>, mut redis_client: redis::aio::MultiplexedConnection, stream_key: String, maxlen: usize) -> () {
+    while let Some((recv_ts, input, addr)) = rx.recv().await {
+        let utf8_input = String::from_utf8_lossy(&input);
+        let parsed_msg = syslog_loose::parse_message_with_year(&utf8_input, resolve_year, syslog_loose::Variant::Either);
+
+        let protocol = match parsed_msg.protocol {
+            syslog_loose::Protocol::RFC3164 => "RFC3164".to_string(),
+            syslog_loose::Protocol::RFC5424(version) => format!("RFC5424_v{}", version),
+        };
+        let mut items = vec![
+            ("recv_ts".to_string(), recv_ts.to_rfc3339_opts(chrono::SecondsFormat::Micros, true)),
+            ("addr".to_string(), addr.to_string().into()),
+            ("protocol".to_string(), protocol),
+        ];
+        if let Some(facility) = parsed_msg.facility {
+            items.push(("facility".to_string(), facility.as_str().to_string()));
+        }
+        if let Some(severity) = parsed_msg.severity {
+            items.push(("severity".to_string(), severity.as_str().to_string()));
+        }
+        if let Some(ts) = parsed_msg.timestamp {
+            items.push(("ts".to_string(), ts.to_rfc3339_opts(chrono::SecondsFormat::Micros, true)));
+        }
+        if let Some(hostname) = parsed_msg.hostname {
+            items.push(("hostname".to_string(), hostname.to_string()));
+        }
+        if let Some(appname) = parsed_msg.appname {
+            items.push(("appname".to_string(), appname.to_string()));
+        }
+        if let Some(procid) = parsed_msg.procid {
+            match procid {
+                syslog_loose::ProcId::PID(pid) => items.push(("proc_pid".to_string(), format!("{}", pid))),
+                syslog_loose::ProcId::Name(name) => items.push(("proc_name".to_string(), name.to_string())),
+            }
+        }
+        if let Some(msg_id) = parsed_msg.msgid {
+            items.push(("msgid".to_string(), msg_id.to_string()));
+        }
+        if !parsed_msg.structured_data.is_empty() {
+            let mut json_elements = Vec::with_capacity(parsed_msg.structured_data.len());
+            for data_entry in parsed_msg.structured_data {
+                let mut json_params = Vec::with_capacity(data_entry.params.len());
+                for param in data_entry.params {
+                    json_params.push(json!({ param.0: param.1}));
+                }
+
+                json_elements.push(json!({
+                    "id": data_entry.id,
+                    "params": json_params,
+                }));
+            }
+
+            items.push(("data".to_string(), json!(json_elements).to_string()));
+        }
+        items.push(("msg".to_string(), parsed_msg.msg.to_string()));
+
         let result= redis_client.xadd_maxlen(&stream_key, StreamMaxlen::Approx(maxlen), "*", &items).await;
         match result {
             Ok(()) => (),
@@ -66,13 +126,14 @@ fn main() -> Result<()> {
         let client = redis::Client::open(redis_conn_info).unwrap();
         let conn = client.get_multiplexed_async_connection().await?;
 
-        let (tx, rx) = mpsc::channel::<(Vec<u8>, SocketAddr)>(1_000);
+        let (tx, rx) = mpsc::channel::<(chrono::DateTime<chrono::Utc>, Vec<u8>, SocketAddr)>(1_000);
         tokio::spawn(syslog_worker(rx, conn, stream_key, *maxlen));
 
         let mut buf = [0; 1024];
         loop {
             let (len, addr) = socket.recv_from(&mut buf).await?;
-            tx.send((buf[..len].to_vec(), addr)).await?;
+            let recv_ts = chrono::Utc::now();
+            tx.send((recv_ts, buf[..len].to_vec(), addr)).await?;
         }
     })
 }
